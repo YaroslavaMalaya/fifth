af310383199b13698b85419dfbb6c648b2d4f996
Yaroslava Mala <ymala@kse.org.ua>
2023-04-25 21:17:55 +0300

    preparation

---
+++ lab5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter your text:");
+var input = Console.ReadLine().Split(" ");
+var punctuation = new char[] {',', '.', '?', '!', ';', ':', '"'};
+var words = new List<string>();
+
+foreach (var word in input)
+{
+    words.Add(new string(word.Where(c => !punctuation.Contains(c)).ToArray()));
+    // .ToString() одразу не працює, тому спочатку в Array, а потім формуємо string()
+    //Console.WriteLine(word.Where(c => !punctuation.Contains(c)).ToArray());
+}
+
+foreach (var word in words)
+{
+    Console.WriteLine(word); // це так, щоб ми побачили що воно працює
+}
+
