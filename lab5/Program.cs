4cda2679fd1b508830cdc09b4730fef35c378b02
smazepaa <smazepa@kse.ord.ua>
2023-04-27 14:41:43 +0300

    checking and printing the typos

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Enter your text:");
+﻿Console.Write("> Enter your text: ");
 var input = Console.ReadLine().Split(" ");
 var punctuation = new char[] {',', '.', '?', '!', ';', ':', '"'};
 var words = new List<string>();
@@ -15,3 +15,25 @@ foreach (var word in words)
     Console.WriteLine(word); // це так, щоб ми побачили що воно працює
 }
 
+List<string> spellChecker = new List<string>();
+foreach (var line in File.ReadAllLines("words_list.txt"))
+{
+    spellChecker.Add(line.ToLower());
+}
+List<string> wrongWords = new List<string>();
+foreach (var word in words)
+{
+    if (!spellChecker.Contains(word.ToLower()))
+    {
+        wrongWords.Add(word);
+    }
+}
+
+Console.Write("< Looks like you don't know how to type: ");
+Console.WriteLine(string.Join(", ", wrongWords));
+/*
+foreach (var word in wrongWords)
+{
+    Console.Write($"'{word}', "); // another option that looks like 'word1', 'word2', 
+}
+*/
