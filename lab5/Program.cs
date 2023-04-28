ce3f3a734d466296b56c6fbead23c87713aa3c18
Yaroslava Mala <ymala@kse.org.ua>
2023-04-28 22:22:06 +0300

    added matrix with indexes in first row and first column

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -37,3 +37,34 @@ foreach (var word in wrongWords)
     Console.Write($"'{word}', "); // another option that looks like 'word1', 'word2', 
 }
 */
+
+var toyvo = new Dictionary<string, int>(); // слова підсказки та відстань до нього від неправильного
+var check = 0; // щоб вивести в косоль для прикладу n-ну кількість матриць
+foreach (var wrong in wrongWords)
+{
+    var wrongLnght = wrong.Length;
+    foreach (var word in File.ReadAllLines("words_list.txt"))
+    {
+        check++;
+        var wordLnght = word.Length;
+        var tseyvo = new int[wrongLnght + 1, wordLnght + 1]; // матриця
+        for (int j = 0; j <= wordLnght; j++)
+            tseyvo[0, j] = j; // прописує рядок з індексами
+        for (int i = 0; i <= wrongLnght; i++)
+            tseyvo[i, 0] = i; // прописує стовбець з індексами
+
+        // частина знизу виводить у консоль перші два слова з words_list, щоб побачити що воно правильне 
+        if (check <= 2)
+        {
+            Console.WriteLine(word);
+            for (int i = 0; i <= wrongLnght; i++)
+            {
+                for (int j = 0; j <= wordLnght; j++)
+                {
+                    Console.Write(tseyvo[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
