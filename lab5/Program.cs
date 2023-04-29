e88fc42f26becfa69f3c523b7c3e1e4530f12980
smazepaa <smazepa@kse.ord.ua>
2023-04-29 13:10:52 +0300

    levenshtein distance (w/o swapping), printing 5 closest words

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -10,10 +10,6 @@ foreach (var word in input)
     //Console.WriteLine(word.Where(c => !punctuation.Contains(c)).ToArray());
 }
 
-foreach (var word in words)
-{
-    Console.WriteLine(word); // це так, щоб ми побачили що воно працює
-}
 
 List<string> spellChecker = new List<string>();
 foreach (var line in File.ReadAllLines("words_list.txt"))
@@ -29,49 +25,63 @@ foreach (var word in words)
     }
 }
 
+var distances = new Dictionary<string, int>();
+
 if (wrongWords.Count != 0)
 {
     Console.Write("< Looks like you don't know how to type: ");
     Console.WriteLine(string.Join(", ", wrongWords));
+    
+    foreach (var wrong in wrongWords)
+    {
+        foreach (var word in File.ReadAllLines("words_list.txt"))
+        {
+            distances[word] = LevenshteinDistance(wrong, word);
+        }
+
+        var sortedKeyValuePairs = distances.OrderBy(x => x.Value).Take(5).ToList();
+        var suggestions = new List<string>();
+
+        foreach (var pair in sortedKeyValuePairs) suggestions.Add(pair.Key);
+
+        Console.Write($"< possible suggestions for {wrong}: ");
+        Console.WriteLine(string.Join(", ", suggestions));
+    }
 }
 else
 {
     Console.WriteLine("< At least you have a dictionary at home. Congrats.");
 }
-/*
-foreach (var word in wrongWords)hek
-{
-    Console.Write($"'{word}', "); // another option that looks like 'word1', 'word2', 
-}
-*/
 
-var toyvo = new Dictionary<string, int>(); // слова підсказки та відстань до нього від неправильного
-var check = 0; // щоб вивести в косоль для прикладу n-ну кількість матриць
-foreach (var wrong in wrongWords)
+
+int LevenshteinDistance(string word1, string word2)
 {
-    var wrongLnght = wrong.Length;
-    foreach (var word in File.ReadAllLines("words_list.txt"))
+    var w1 = word1.Length;
+    var w2 = word2.Length;
+
+    var matrix = new int[w1 + 1, w2 + 1]; 
+    
+    for (int j = 0; j <= w2; j++)
+        matrix[0, j] = j; // прописує рядок з індексами
+    for (int i = 0; i <= w1; i++)
+        matrix[i, 0] = i; // прописує стовбець з індексами
+
+    for (var j = 1; j <= w2; j++)
     {
-        check++;
-        var wordLnght = word.Length;
-        var tseyvo = new int[wrongLnght + 1, wordLnght + 1]; // матриця
-        for (int j = 0; j <= wordLnght; j++)
-            tseyvo[0, j] = j; // прописує рядок з індексами
-        for (int i = 0; i <= wrongLnght; i++)
-            tseyvo[i, 0] = i; // прописує стовбець з індексами
-
-        // частина знизу виводить у консоль перші два слова з words_list, щоб побачити що воно правильне 
-        if (check <= 2)
+        for (var i = 1; i <= w1; i++)
         {
-            Console.WriteLine(word);
-            for (int i = 0; i <= wrongLnght; i++)
-            {
-                for (int j = 0; j <= wordLnght; j++)
-                {
-                    Console.Write(tseyvo[i, j] + " ");
-                }
-                Console.WriteLine("");
-            }
+            var cost = word1[i - 1] == word2[j - 1] ? 0 : 1;
+            
+            /* the same as
+             if (word1[j - 1] == word2[i - 1])
+                cost = 0;
+            else cost = 1;
+            */
+
+            matrix[i, j] = Math.Min(
+                Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                matrix[i - 1, j - 1] + cost);
         }
     }
+    return matrix[w1 , w2];
 }
